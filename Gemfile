2129b1b259ef149d5d989689accb104991e70264
Doug MacDowell <dougmac9@uw.edu>
2014-01-21 12:24:19 -0800

    Moved project to new location

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+gem 'sqlite3'
+gem 'sass-rails', '~> 4.0.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 1.2'
+gem 'turbolinks'
+gem 'uglifier', '>= 1.3.0'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :development, :test do
+  gem 'minitest-rails'
+end
+
+group :test do
+  gem 'minitest-rails-capybara'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portfolio::Application.config.secret_key_base = 'f83eaa8922772325eb4bd46bc254c3df3542e2ffaad9f5e56e1451ca569d108838f1a4c217dcc04a9aec0b4de972bac5c2c03f58cff6f8ac7526b52e2f1c6785'
