ef67cb4520d10974d949e6f8973e7714e9e01536
Doug MacDowell <dougmac9@uw.edu>
2014-01-27 20:18:18 -0800

    Modified feature tests to use fixtures

--- test/features/deleting_a_post_test.rb
+++ test/features/deleting_a_post_test.rb
@@ -3,11 +3,11 @@ require "test_helper"
 feature "Deleting A Post" do
   scenario "the post is deleted with a click" do
     # Given an existing post
-#    Post.create(title: "Becoming a Code Fellow", body: "Means striving for excellence.")
+    Post.create(title: "Becoming a Code Fellow", body: "Means striving for excellence.")
     visit posts_path
 
     # When the delete link is clicked
-    click_on "Destroy"
+    page.find("tr:last").click_on "Destroy"
 
     # Then the post is deleted
     page.wont_have_content "Becoming a Code Fellow"
