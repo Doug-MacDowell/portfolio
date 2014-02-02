f67a637710ef2fbde176c99bba7e66ea57e7f1b4
Doug MacDowell <dougmac9@uw.edu>
2014-02-01 16:44:43 -0800

    Added a working test. Fixes #15.

--- test/features/check_for_zurb_test.rb
+++ test/features/check_for_zurb_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 
 feature "CheckForZurb" do
-#  Capybara.ignore_hidden_elements = false
   scenario "the page has columns" do
     visit root_path
     page.must_have_content "columns"
