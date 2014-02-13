07633b4fdf6b31e28689c7dda3a71d857817134c
Doug MacDowell <dougmac9@uw.edu>
2014-02-13 12:47:41 -0800

    recreated the spec for editing

---
+++ test/features/projects/editing_a_project_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+feature "As the site owner, I want to add a project so that I can correct typos" do
+  scenario "editing an existing project" do
+    # Given an existing project
+    visit edit_project_path(projects(:portfolio))
+    # When I make changes
+    fill_in "Name", with: "My Portfolio"
+    click_on "Update Project"
+    # Then the changes should be saved and displayed
+    page.text.must_include "success"
+    page.text.must_include "My Portfolio"
+    page.text.wont_include "Some Other Portfolio"
+  end
+end
