83f010cd2f2c3e52c32673cea1f168d682652981
Doug MacDowell <dougmac9@uw.edu>
2014-03-16 19:36:40 -0700

    now has Scope class and more. Fixes #22

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,13 +1,38 @@
-class PostPolicy
-  attr_accessor :user, :post
-
-  def initialize(user, post)
-    @user = user
-    @post = post
+class PostPolicy < ApplicationPolicy
+  class Scope < Struct.new(:user, :scope)
+    def resolve
+      if user
+        return scope.where(author_id: user.id) if user.author?
+        return scope.all if user.editor?
+      end
+      scope.where(published: true)
+    end
   end
 
-  def publish?
-    @user.role == "editor"
-  end
+    def initialize(user, post)
+      @user = user || NullUser.new
+      @post = post
+    end
 
+    def permitted_attributes
+      return [] if user.nil?
+      attributes = [:title, :body]
+      attributes << [:published] if @user.editor?
+    end
+
+    def update?
+      @post.author == @user || @user.editor?
+    end
+
+    def create?
+      @user.author? || @user.editor?
+    end
+
+    def publish?
+      @user.editor?
+    end
+
+    def destroy?
+      @user.editor?
+    end
 end
