9ea68f63e20b7e73906078c1d5305c3c1bbe71fd
Doug MacDowell <dougmac9@uw.edu>
2014-04-05 14:01:38 -0700

    changed post_params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,13 +34,12 @@ class PostsController < ApplicationController
         current_user.posts << @post
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render action: 'show', status: :created, location: @post }
+        current_user.posts << @post
       else
         format.html { render action: 'new' }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
-      # current_user.posts << @post
     end
-    # current_user.posts << @post
   end
 
   # PATCH/PUT /posts/1
@@ -48,7 +47,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        current_user.posts << @post
+      #  current_user.posts << @post
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
         format.json { head :no_content }
       else
@@ -76,8 +75,8 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(policy(Post).permitted_attributes)
-    # params.require(:post).permit(:title, :body, (:published if current_user.role == "editor"))
+      params.require(:post).permit(:title, :body, (:published if current_user.role == "editor"))
+      #params.require(:post).permit(policy(Post).permitted_attributes)
     end
 
 
