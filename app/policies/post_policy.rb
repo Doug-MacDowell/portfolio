63acda2c6af9e58ac7490989549c2bb7d13a3684
Doug MacDowell <dougmac9@uw.edu>
2014-04-05 17:13:42 -0700

    added Scope and changed permitted attr method

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,39 +1,32 @@
 class PostPolicy < ApplicationPolicy
-  #class PostPolicy < Struct.new(:user, :post)
-
-  attr_reader :user, :post
-
-#  class Scope < Struct.new(:user, :scope)
- #   def resolve
- #     if user
- #       return scope.where(author_id: user.id) if user.author?
- #       return scope.all if user.editor?
- #     end
- #     scope.where(published: true)
- #   end
- # end
+  class Scope < Struct.new(:user, :scope)
+    def resolve
+      if user
+        return scope.where(author_id: user.id) if user.author?
+        return scope.all if user.editor?
+      end
+      scope.where(published: true)
+    end
+  end
 
     def initialize(user, post)
-      @user = user
+      @user = user || NullUser.new
       @post = post
     end
 
     def permitted_attributes
-   #  if user.editor? || user.owner_of?(post)
-      if user.editor?
-        [:title, :body, :published, :tag_list]
-      else
-        [:tag_list]
-        #return [] if user.nil?
-      end
+      return [] if user.nil?
+      attributes = [:title, :body]
+      attributes << [:published] if @user.editor?
     end
 
     def update?
-      @user.author? || @user.editor?
+      @post.author == @user || @user.editor?
+      #user.admin? or not post.published?
     end
 
     def create?
-      @user.author?
+      @user.author? || @user.editor?
     end
 
     def publish?
@@ -42,11 +35,5 @@ class PostPolicy < ApplicationPolicy
 
     def destroy?
       @user.editor?
-    end
-
-    def index?
-      if ! @user.nil?
-        @user.author? || @user.editor?
-      end
     end
 end
