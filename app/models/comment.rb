7f149c659c9dff9ade311a8db009a90f563520e6
Doug MacDowell <dougmac9@uw.edu>
2014-04-06 15:05:52 -0700

    added some code to the files that were generated for comment resource

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,10 @@
 class Comment < ActiveRecord::Base
-  belongs_to :post
+#  belongs_to :post
+
+  belongs_to :post, :commentable, polymorphic: true
+
+  validates :content, presence: true
+
+  scope :approved, -> { where(approved: true) }
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ActiveRecord::Base
   belongs_to :author, class_name: "User"
 
+#  has_many :comments, as: :commentable
+#  scope :published, -> { where(published: true) }
+
   def publish!
     publish = true
     save!
