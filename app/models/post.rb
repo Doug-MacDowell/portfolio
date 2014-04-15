882064c43c0c143c6d75e37e004d860a9662365d
Doug MacDowell <dougmac9@uw.edu>
2014-04-14 23:22:32 -0700

    removed attr accessible lines

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content
+ # attr_accessible :content  # not needed with strong_params
 
   belongs_to :author, class_name: "User"
   has_many :comments, as: :commentable
