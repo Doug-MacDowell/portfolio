d3e43fdad508c9bc8515e2e7a86c96139a8a4564
Doug MacDowell <dougmac9@uw.edu>
2014-04-14 22:36:50 -0700

    added index and update methods, now uses commentable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,60 @@
 class CommentsController < ApplicationController
+  before_filter :load_commentable, except: [:index]
+
+   def index
+     authorize Comment
+     @comments = Comment.all
+   end
 
    def create
      @comment = Comment.new(comment_params)
      if @comment.save
        flash[:notice] = "This comment is awaiting moderation"
-    #   redirect_to post_path(@comment.post_id)
-       redirect_to posts_path
+       redirect_to @commentable
+    #   redirect_to posts_path  # old way
      else
-       @post = Post.find(@comment.post_id)
-       render template: "posts/show"
+       instance_variable_set("@#{@resource.singularize}".to_sym, @commentable)
+       render template: "#{@resource}/show"
+    #   @post = Post.find(@comment.post_id)  #old way
+    #   render template: "posts/show"  #old way
      end
    end
 
+  def update
+    if @commentable.class == Comment
+      @comment_entity = @commentable
+    else
+      @comment_entity = @commentable.comment
+    end
+
+    if @comment_entity.update_attributes(comment_params)
+      flash[:notice] = "Comment has been updated"
+      if @comment_entity.class == Comment
+        redirect_to comments_path
+      else
+        redirect_to @commentable
+      end
+    else
+      instance_variable_set("@#{@resource.singularize}".to_sym, @commentable)
+      render template: "#{@resource}/show"
+    end
+  end
+
   private
 
-   def comment_params
+  def comment_params
      params.require(:comment).permit(:author,
                                     :author_url,
                                     :author_email,
                                     :content,
                                     :referrer,
                                     :post_id)
-   end
+  end
+
+
+  def load_commentable
+    @resource, id = request.path.split('/')[1,2]
+    @commentable = @resource.singularize.classify.constantize.find(id)
+  end
 
 end
