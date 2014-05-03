6d3357a74a71d89b4c34746ecfef9304635c0dd1
Doug MacDowell <dougmac9@uw.edu>
2014-05-03 14:31:23 -0700

    messed up travis when I removed this

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,13 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+Portfolio::Application.config.secret_key_base = '945ffb644a5cbb69fd8bc5cf851fc39d5bbc6eb8f155fd78b89df227a5228eec3a4b39202b1e1f91c14216dc65112a9aaada889976e930787a4c4fccd8bf4963'
