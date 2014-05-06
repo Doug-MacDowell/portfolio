4e58d73cf098fb966230c13486f3f7f74872278d
Doug MacDowell <dougmac9@uw.edu>
2014-05-05 17:26:49 -0700

    schema update for images and removed after_action verify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  #after_action :verify_policy_scoped, :only => :index
-  #after_action :verify_authorized, :except => :index
-
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
