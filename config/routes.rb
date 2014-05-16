eca91b9ba760bcb934b02eed33f449340a6a7223
Doug MacDowell <dougmac9@uw.edu>
2014-05-15 19:36:49 -0700

    add a messages resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Portfolio::Application.routes.draw do
                            passwords: "users/passwords",
                            omniauth_callbacks: "omniauth_callbacks"}
 
+  require 'sidekiq/web'
+  mount Sidekiq::Web, at: '/sidekiq'
+
   resources :posts do
     resources :comments
   end
@@ -14,6 +17,8 @@ Portfolio::Application.routes.draw do
   root 'home#index'
 
   resources :comments
+
+  resources :messages
 
   namespace :api do
     namespace :v1 do
