615cd68f51eb02adb4498b3829e53bfc97c18e3e
Doug MacDowell <dougmac9@uw.edu>
2014-05-15 21:00:36 -0700

    added flash message if error happens

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,10 +7,11 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(params[:message])
     if @message.valid?
-      # TODO send message goes here...
+      flash[:error] = nil
       flash[:notice] = "Message sent! Thanks for contacting us."
       redirect_to root_url
     else
+      flash[:error] = 'Cannot send message.'
       render :action => 'new'
     end
   end
