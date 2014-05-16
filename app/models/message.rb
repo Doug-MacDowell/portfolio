4d392fc9cc9a811c89a6601db5b4f3b41c50b406
Doug MacDowell <dougmac9@uw.edu>
2014-05-15 21:02:04 -0700

    cleaned out some commented lines

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,7 +6,7 @@ class Message
   attr_accessor :name, :email, :content
 
   validates_presence_of :name
-  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+  validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
   validates_length_of :content, :maximum => 500
 
   def initialize(attributes = {})
